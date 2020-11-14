baf818940deba81ebc18e04262a8a04e7c3e6dbc
Aaron Sexton <asexton7@murraystate.edu>
2020-11-14 14:22:36 -0500

    added functions for user input, added refresh to remove popup

--- flight_scrape.py
+++ flight_scrape.py
@@ -1,45 +1,70 @@
 # import libraries
 from time import sleep, strftime
-from random import randint
 import pandas as pd
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import smtplib
 from email.mime.multipart import MIMEMultipart
 
 # setup driver
-browser = webdriver.Firefox()
+CHROMEDRIVER_PATH = "D:\\Programming\\ChromeDriver\\chromedriver.exe"
+# chrome_options = Options()
+# chrome_options.add_argument("--headless")
+browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH) #, options=chrome_options)
 
-# get airport designations
-origin = input("Enter 3 letter origin airport: ")
-destination = input("Enter 3 letter destination airport: ")
 
-# get dates
-departure = input("Enter departure date as 'yyyy-mm-dd': ")
-return_flight = input("Enter return date as 'yyyy-mm-dd': ")
+def getPorts():
+    # get airport designations
+    origin = input("Enter 3 letter origin airport: ")
+    while(len(origin) != 3):
+        origin = ...
+        input("Only 3 letter airport codes supported, try again: ")
+    destination = input("Enter 3 letter destination airport: ")
+    while(len(destination) != 3):
+        destination = ...
+        input("Only 3 letter airport codes supported, try again: ")
 
-# request flight type
-type = input("Enter flight type (best, cheapest, or quickest): ")
-if(type == 'best'):
-    type_in = '?sort=bestflight_a'
-elif(type == 'cheapest'):
-    type_in = '?sort=price_a'
-elif(type == 'quickest'):
-    type_in = '?sort=duration_a'
+    return origin, destination
+
+
+def getDates():
+    # get dates
+    depart_date = input("Enter departure date as 'yyyy-mm-dd': ")
+    while(len(depart_date) != 10):
+        depart_date = input("Improper date format, ensure date format is 'yyyy-mm-dd': ")
+
+    return_date = input("Enter return date as 'yyyy-mm-dd': ")
+    while(len(return_date) != 10):
+        return_date = input("Improper date format, ensure date format is 'yyyy-mm-dd': ")
+
+    return [depart_date, return_date]
+
+
+def getFlightType():
+    # request flight type
+    type = input("Enter flight type (best, cheapest, or quickest): ")
+    while(type != ('best' or 'cheapest' or 'quickest')):
+        type = input("Input was not 'best, cheapest, or quickest', retry: ")
+    if(type == 'best'):
+        type_in = '?sort=bestflight_a'
+    elif(type == 'cheapest'):
+        type_in = '?sort=price_a'
+    elif(type == 'quickest'):
+        type_in = '?sort=duration_a'
+
+    return type_in
+
+
+# get airports, dates, and flight type
+origin, destination = getPorts()
+depart_date, return_date = getDates()
+type = getFlightType()
 
 # open the corresponding browser page to check for updates
-kayak = 'https://www.kayak.com/flights/' + origin + '-' + destination + '/' + departure + '/' + return_flight + type_in
+kayak = 'https://www.kayak.com/flights/{}-{}/{}/{}{}'.format(origin, destination, depart_date, return_date, type)
 browser.get(kayak)
 sleep(5)
 
-try:
-    xp_popup_close = '//button[contains(@id,"dialog-close") and contains(@class,"Button-No-Standard-Style close ")]'
-    browser.find_elements_by_xpath(xp_popup_close)[5].click()
-except Exception as e:
-    pass
-
-prices_x = '//a[@class="booking-link"]/span[@class="price option-text"]'
-prices = browser.find_elements_by_xpath(prices_x)
-prices_list = [price.text.replace('$', '') for price in prices if price.text != '']
-prices_list = list(map(int, prices_list))
-print(prices_list)
+# refresh to close pop up
+browser.refresh()
